900df63cf1cb7dd68686f370683f629fd853ce0a
Khushnood <khushnoodhussain@fynd-external.com>
2022-11-17 13:07:33 +0530

    amended

--- src/util/axiosConfig.js
+++ src/util/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://kkchatapp.herokuapp.com'
   });
 
 export default axiosInstance
